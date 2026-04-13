1539ae79e1f37c45de1ef2372e03634a92e515f7
wilson <hereisleedo@gmail.com>
2026-04-13 18:15:01 +0800

    version2

--- src/test/java/com/pawsstay/auth_service/AuthServiceApplicationTests.java
+++ src/test/java/com/pawsstay/auth_service/AuthServiceApplicationTests.java
@@ -3,7 +3,9 @@ package com.pawsstay.auth_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"OWNER_SERVICE_URL=localhost:8081"
+})
 class AuthServiceApplicationTests {
 
 	@Test
